d96712754045bc71eac5b6f35068426cf219d9f1
Vinicius Dourado <vinidourado87@gmail.com>
2017-05-03 21:03:19 -0300

    Start to creat REST services

--- src/main/java/com/management/controller/SchedulersController.java
+++ src/main/java/com/management/controller/SchedulersController.java
@@ -1,22 +1,36 @@
 package com.management.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.management.model.Schedule;
+import com.management.service.ScheduleService;
 
 @RestController
 @RequestMapping("/schedulers")
 public class SchedulersController {
 
-	@RequestMapping("/")
-    public List<String> list() {
-        return Arrays.asList("list");
+	@Autowired
+	private ScheduleService service;
+	
+	@RequestMapping("")
+    public List<Schedule> list() {
+        return service.list();
+    }
+
+	@RequestMapping("/{id}")
+    public Schedule find(@PathVariable("id") Long id) {
+        return service.find(id);
     }
 	
-	@RequestMapping("/{id}")
-    public List<String> find() {
-        return Arrays.asList("list");
-    }
+	@PostMapping("")
+	public void create(@RequestBody Schedule schedule) {
+		service.save(schedule);
+	}
 }
--- src/main/java/com/management/service/ScheduleService.java
+++ src/main/java/com/management/service/ScheduleService.java
@@ -1,8 +1,11 @@
 package com.management.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.management.model.Schedule;
 import com.management.repository.ScheduleRepository;
 
 @Service
@@ -11,4 +14,15 @@ public class ScheduleService {
 	@Autowired
 	private ScheduleRepository scheduleRepository;
 
+	public Schedule find(Long id) {
+		return scheduleRepository.findOne(id);
+	}
+	
+	public List<Schedule> list() {
+		return scheduleRepository.findAll();
+	}
+	
+	public Schedule save(Schedule schedule) {
+		return scheduleRepository.save(schedule);
+	}
 }
